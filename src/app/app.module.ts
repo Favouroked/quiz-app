4d2045225d08da337e2315c267fc2501e2d18f33
Favour Okedele <favouroked@gmail.com>
2018-05-25 23:23:50 -0400

    changed the frontend of the quiz and implemented the functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { QuizComponent } from './quiz/quiz.component';
 import {GetQuizService} from './get-quiz.service';
+import { QuizFormComponent } from './quiz-form/quiz-form.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'add-quiz', component: QuizFormComponent},
+  {path: 'quiz/:id', component: QuizComponent}
+];
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuizComponent
+    QuizComponent,
+    QuizFormComponent,
+    HomeComponent
+  ],
+  schemas: [
+    NO_ERRORS_SCHEMA
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [
     GetQuizService
--- src/app/quiz/quiz.component.ts
+++ src/app/quiz/quiz.component.ts
@@ -10,8 +10,13 @@ import {QuizQuestion} from '../models/quiz';
 export class QuizComponent implements OnInit {
   questions: QuizQuestion[];
   correct: boolean;
+  answer: string;
+  displayInfo: boolean;
   question: QuizQuestion;
   counter = 0;
+  displayAnswer: boolean;
+  endquiz: boolean;
+  score: number;
 
   constructor(public qs: GetQuizService) {
   }
@@ -19,19 +24,45 @@ export class QuizComponent implements OnInit {
   ngOnInit() {
     this.questions = this.qs.getQuestions();
     this.question = this.questions[this.counter];
+    this.displayAnswer = false;
+    this.endquiz = false;
+    this.score = 0;
+  }
+
+  endQuiz() {
+    this.endquiz = true;
   }
 
   nextQuestion() {
     ++this.counter;
     this.question = this.questions[this.counter];
+    this.displayAnswer = false;
+    this.displayInfo = false;
+    if (this.counter >= this.questions.length) {
+      this.endQuiz();
+    }
   }
 
   isAnswer(answerIndex): void {
     const quest = this.questions[this.counter];
+    const ansIndex = quest.answer;
     if (answerIndex == quest.answer) {
       this.correct = true;
+      this.displayAnswer = true;
+      ++this.score;
+      setTimeout(() => {
+        console.log('Right answer');
+        this.nextQuestion();
+      }, 3000);
     } else {
       this.correct = false;
+      this.answer = quest.options[ansIndex];
+      this.displayInfo = true;
+      this.displayAnswer = true;
+      setTimeout(() => {
+        console.log('Wrong answer');
+        this.nextQuestion();
+      }, 3000);
     }
   }
 }
